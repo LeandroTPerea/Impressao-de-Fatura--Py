c8e273343b2afe2cdeadfcc1c8eecfa8f905adad
Le T.P <101605589+PlayerZero1@users.noreply.github.com>
2022-09-11 16:51:44 -0300

    Create Exercicio01-Impressão de Fatura.py

---
+++ Exercicio01-Impressão de Fatura.py
@@ -0,0 +1,10 @@
+cliente=input("Digite o nome do cliente:")
+
+dia_vencimento=int(input("Digite o dia de vencimento:"))
+
+mês_vencimento=input("Digite o mês de vencimento:")
+
+fatura_valor=input("Digite o valor da fatura:")
+
+print("Olá,",cliente)
+print ("A sua fatura com vencimento em",dia_vencimento,"de",mês_vencimento,"no valor de R$",fatura_valor,"está fechada.")
